30d8badec3a399b88dd626a9832ed0358241312b
bbulatao <bbulatao@stevens.edu>
2025-03-31 23:53:58 -0400

    hope

--- borgir-app/src/components/Navbar.jsx
+++ borgir-app/src/components/Navbar.jsx
@@ -1,52 +1,49 @@
 import { Link } from "react-router-dom";
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
+import { useAuth, SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
 
 export default function NavBar() {
-  // const { signOut } = useAuth();
+  const { signOut } = useAuth();
+  return (
+    <nav className="p-4 bg-yellow-500 flex justify-between items-center">
+      <h1 className="text-xl font-bold">Borgir 🍔</h1>
+      <div>
+        <Link to="/" className="mr-4">
+          Home
+        </Link>
 
-  return <h1>Bruh</h1>;
+        <Link to="/forum" className="mr-4">
+          Forum
+        </Link>
 
-  // return (
-  //   <nav className="p-4 bg-yellow-500 flex justify-between items-center">
-  //     <h1 className="text-xl font-bold">Borgir 🍔</h1>
-  //     <div>
-  //       <Link to="/" className="mr-4">
-  //         Home
-  //       </Link>
+        <Link to="/reviews" className="mr-4">
+          Reviews
+        </Link>
 
-  //       <Link to="/forum" className="mr-4">
-  //         Forum
-  //       </Link>
+        <SignedIn>
+          <Link to="/dashboard" className="mr-4">
+            Dashboard
+          </Link>
 
-  //       <Link to="/reviews" className="mr-4">
-  //         Reviews
-  //       </Link>
+          <Link to="/post" className="mr-4">
+            Post
+          </Link>
 
-  //       <SignedIn>
-  //         <Link to="/dashboard" className="mr-4">
-  //           Dashboard
-  //         </Link>
+          <button onClick={() => signOut()} className="bg-red-500 px-3 py-1 rounded text-white">
+            Logout
+          </button>
 
-  //         <Link to="/post" className="mr-4">
-  //           Post
-  //         </Link>
+          <UserButton className="px-3 mx-4" />
+        </SignedIn>
 
-  //         <button onClick={() => signOut()} className="bg-red-500 px-3 py-1 rounded text-white">
-  //           Logout
-  //         </button>
-
-  //         <UserButton className="px-3 mx-4" />
-  //       </SignedIn>
-
-  //       <SignedOut>
-  //         <SignInButton
-  //           mode="modal"
-  //           className="bg-blue-500 px-3 py-1 rounded text-white cursor-pointer"
-  //         >
-  //           <button>Sign In</button>
-  //         </SignInButton>
-  //       </SignedOut>
-  //     </div>
-  //   </nav>
-  // );
+        <SignedOut>
+          <SignInButton
+            mode="modal"
+            className="bg-blue-500 px-3 py-1 rounded text-white cursor-pointer"
+          >
+            <button>Sign In</button>
+          </SignInButton>
+        </SignedOut>
+      </div>
+    </nav>
+  );
 }
