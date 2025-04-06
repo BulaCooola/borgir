ecc20038ee80aa37f113d290174b525fa49a579f
bbulatao <bbulatao@stevens.edu>
2025-04-06 16:36:31 -0400

    Update vite.config.js

--- borgir-app/vite.config.js
+++ borgir-app/vite.config.js
@@ -5,14 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  preview: {
-    port: 8080,
-    strictPort: true,
-  },
-  server: {
-    port: 8080,
-    strictPort: true,
-    host: true,
-    origin: "http://0.0.0.0:8080",
-  },
+  config: JSON.stringify({
+    apiUrl: "/api",
+  }),
 });
