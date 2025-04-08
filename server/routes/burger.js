e39485feb63f6496a1f8291d4ede6aabf017fe21
bbulatao <bbulatao@stevens.edu>
2025-04-07 23:44:59 -0400

    pls work

--- server/routes/burger.js
+++ server/routes/burger.js
@@ -24,6 +24,7 @@ router.post("/", async (req, res) => {
       return res.status(400).json({ error: "Burger already exists in this restaurant" });
     }
 
+    // object to insert
     const newBurger = {
       name,
       restaurant,
